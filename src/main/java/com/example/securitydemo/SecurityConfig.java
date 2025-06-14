1ad22d7438a156654e54123c494f24221fe27afb
Petri Järvenpää <petri.jarvenpaa@outlook.com>
2025-06-14 22:47:34 +0300

    Role based authentication and H2 configuration

--- src/main/java/com/example/securitydemo/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.example.securitydemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,29 +18,35 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        //http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(headers -> headers
+                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-
-        UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
+        UserDetails user1 = User.withUsername("user")
+                .password("{noop}user")
                 .roles("USER")
                 .build();
-
         UserDetails user2 = User.withUsername("admin")
-                .password("{noop}adminPass")
+                .password("{noop}admin")
                 .roles("ADMIN")
                 .build();
-
         return new InMemoryUserDetailsManager(user1, user2);
     }
+
 }
