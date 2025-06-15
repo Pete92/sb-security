075531e593f127dcd1cc1c6bc78eb1a8c300fd90
Petri Järvenpää <petri.jarvenpaa@outlook.com>
2025-06-15 21:34:58 +0300

    Sing-In flow with JWT

--- src/main/java/com/example/securitydemo/GreetingsController.java
+++ src/main/java/com/example/securitydemo/GreetingsController.java
@@ -1,12 +1,33 @@
 package com.example.securitydemo;
 
-import org.springframework.security.access.prepost.PostAuthorize;
+import com.example.securitydemo.jwt.JwtUtils;
+import com.example.securitydemo.jwt.LoginRequest;
+import com.example.securitydemo.jwt.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class GreetingsController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @GetMapping("/hello")
     public String sayHello(){
@@ -18,9 +39,37 @@ public class GreetingsController {
     public String userEndPoint(){
         return "Hello User!";
     }
+
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String adminEndPoint(){
         return "Hello Admin!";
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest){
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    )
+            );
+        } catch (AuthenticationException e) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("error", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication); // Set authenticated user to context
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        LoginResponse response = new LoginResponse(jwtToken, userDetails.getUsername(), roles);
+        return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 }
